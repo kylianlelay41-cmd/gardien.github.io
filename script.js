bd7ac57b2fc0c1604357e5a217efe28a5badba71
kylianlelay41-cmd <kylianlelay41@gmail.com>
2026-02-04 00:11:25 +0100

    yooolooo

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function openModal(image, title, desc) {
+function openModal(image, title, desc, gif) {
   document.getElementById("modal").style.display = "flex";
   document.getElementById("modal-img").src = image;
   document.getElementById("modal-title").textContent = title;
   document.getElementById("modal-desc").textContent = desc;
+
+  const gifEl = document.getElementById("modal-gif");
+
+  if (gif) {
+    gifEl.src = gif;
+    gifEl.style.display = "block";
+  } else {
+    gifEl.style.display = "none";
+  }
 }
 
 function closeModal() {
